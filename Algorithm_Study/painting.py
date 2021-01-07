e47ee476dccd3a8db85e272d4400ea831fdeb3ed
bottlesteel97 <gch04602@naver.com>
2021-01-07 21:55:53 +0900

    Update painting.py

--- Algorithm_Study/painting.py
+++ Algorithm_Study/painting.py
@@ -1,23 +1,31 @@
-test_case = int(input())
-for i in range(test_case):
-    box_num =  int(input())
-    for j in range(box_num) : 
-        box_info = input().split
-        box_info = [int(k) for k in box_info]
-        box = 
-
-        
 def box_info2grid(box_info ):
     lx, ly = box_info[0], box_info[1]
     rx, ry = box_info[2], box_info[3]
     color = box_info[4]
-    create_box(lx,ly,rx,ry,color)
-    grid[]
+    grid = create_box(lx,ly,rx,ry,color)
+    
 
 
 
 def create_box(lx,ly,rx,ry,color):
     grid = [[0 for col in range(rx)] for row in range(ry)]
-    grid[lx:rx, ly:ry] = color
+    grid[lx:rx][ly:ry] = color
     print(grid)
     return grid 
+
+
+test_case = int(input())
+answer_list = []
+for i in range(test_case):
+    box_num =  int(input())
+    box_list = []
+    for j in range(box_num) : 
+        box_info = input().split()
+        box_info = [int(k) for k in box_info]
+        box = box_info2grid(box_info)
+        box_list.append(box)
+    for box in box_list:
+        print(box)
+
+        
+
