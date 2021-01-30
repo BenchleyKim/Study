818a01358d2ab10d838a257dfa357a232f64e873
bottlesteel97 <gch04602@naver.com>
2021-01-30 19:47:31 +0900

    Update makingColoredPaper.py

--- Algorithm_Study/0130/makingColoredPaper.py
+++ Algorithm_Study/0130/makingColoredPaper.py
@@ -1,23 +1,24 @@
 # BOJ 2630
 
-n = 8
-paper = [] 
-# for _ in range(n):
-#   paper.append(list(map(int, input().split())))
+n = int(input())
+pape = [] 
+for _ in range(n):
+  pape.append(list(map(int, input().split())))
+white = 0
+blue = 0   
 
-pape = [[1, 1, 0, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 0, 0, 0, 1, 1, 1, 1], [0, 1, 0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 
-1]]
-
-
-def isOne(paper,white,blue) :
+def isOne(paper) :
+  global white
+  global blue  
   plen = len(paper)
   psum = sum([sum(p) for p in paper])
   if psum == 0 :
-    white += 1 
-    return True
+    white = white+1 
+    return white
   if psum == (plen ** 2) :
-    blue += 1
-    return True
+    
+    blue = blue+1
+    return blue
   else :
     return False
 
@@ -36,11 +37,7 @@ def paperslice(paper) :
       p1.append(p[:l])
       p2.append(p[l:])
   return [p1,p2,p3,p4]
-    
-  
-# print(paperslice(pape))
-white = 0
-blue = 0 
+
 queue = [pape]
 while True :
   if len(queue) == 0 :
@@ -48,11 +45,10 @@ while True :
     print(blue)
     break
   temp = queue.pop(0)
-  if isOne(temp,white,blue) : print(temp) ;continue
+  if isOne(temp) : continue
   Slices = paperslice(temp)
   for s in Slices:
     queue.append(s)
-
 
 
   
