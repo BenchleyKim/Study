1c951bfa9d51dabdc824b815942edcd9ed9ce125
bottlesteel97 <gch04602@naver.com>
2021-02-05 22:38:33 +0900

    Update BOJ2261.py

--- Algorithm_Study/0205/BOJ2261.py
+++ Algorithm_Study/0205/BOJ2261.py
@@ -1,22 +1,43 @@
 import math
+import sys
+sys.setrecursionlimit(10**9)
 n = int(input())
 pointList = []
 for _ in range(n) :
-  pointList.append(list(map(int,input().split())))
-
+    pointList.append(list(map(int,input().split())))
 pointList.sort()
-print(pointList)
 
 def calDist(a, b):
-  return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+    return int((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
 def devideAndConquer(s, e) :
-  l = e -s
-  if l == 2 :
-    return calDist(pointList[s], pointList[e])
-  if l == 3 :
-    return min(calDist(pointList[s],pointList[e]), calDist(pointList[s],pointList[s+1]),calDist(pointList[s+1], pointList[e]))
-  l //= 2
-  left = devideAndConquer(s, l)
-  right = devideAndConquer(l,e)
-  return devideAndConquer
+    l = e -s
+    if l == 2 :
+        return calDist(pointList[s], pointList[e])
+    if l == 3 :
+        return min(calDist(pointList[s],pointList[e]), calDist(pointList[s],pointList[s+1]),calDist(pointList[s+1], pointList[e]))
+    mid = (s + e) // 2
+    left = devideAndConquer(s, mid)
+    right = devideAndConquer(mid+1,e)
+    d = min(left, right)
+    midx = pointList[mid][0]
+    check = []
+    for i in pointList :
+        if (i[0] - midx) ** 2 <= d :
+            check.append(i)
+    if len(check) <= 1 :
+        return d
+    check.sort(key = lambda x:x[1])
+    for i in range(len(check)-1) :
+        for j in range(i+1, len(check)-1) :
+            if (check[i][1] - check[j][1])**2 > d :
+                break
+            if check[i][0] < midx and check[j][0] < midx : 
+                continue
+            if check[i][0] > midx and check[j][0] > midx :
+                continue
+            d = min(d, calDist(check[i], check[j]))
+    return d            
+s = 0
+e = len(pointList) -1 
+print(devideAndConquer(1, e))
