5559fc4ad6e85a0f0757c261f0fadc67c377630b
bottlesteel97 <gch04602@naver.com>
2021-02-07 14:09:00 +0900

    0207

--- Algorithm_Study/0206/BOJ19622.py
+++ Algorithm_Study/0206/BOJ19622.py
@@ -1,13 +1,12 @@
 
-# N = int(input())
-# meeting = []
+N = int(input())
+meeting = []
 
-# for _ in range(N) :
-#     meeting.append(list(map(int,input().split())))
-N = 6
-meeting = [[10, 40, 80], [30, 60, 100], [50, 80, 70], [70, 100, 100], [90, 120, 40], [110, 140, 50]]
-meeting.sort()
-
+for _ in range(N) :
+    meeting.append(list(map(int,input().split())))
+# N = 6
+# meeting = [[10, 40, 80], [30, 60, 100], [50, 80, 70], [70, 100, 100], [90, 120, 40], [110, 140, 50]]
+# meeting.sort()
 arr = [0] * (N)
 for idx, meet in enumerate(meeting) :
     if idx == 0 :
@@ -19,10 +18,10 @@ for idx, meet in enumerate(meeting) :
         else : 
             arr[idx] = meet[2]
         continue
-    if meet[2] > meeting[idx-1][2] :
-        arr[idx] = arr[idx-2] + meet[2]
-    else : 
+    if arr[idx-1] > arr[idx-2] + meet[2] :
         arr[idx] = arr[idx-1]
+    else : 
+        arr[idx] = arr[idx-2] + meet[2]
+    
 
-print(arr)
 print(arr[N-1])
