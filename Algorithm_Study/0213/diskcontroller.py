f4a7c38cad55b1313ae661c6ef142d725d4afc6a
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-02-13 18:29:49 +0900

    Update diskcontroller.py

--- Algorithm_Study/0213/diskcontroller.py
+++ Algorithm_Study/0213/diskcontroller.py
@@ -1,20 +1,34 @@
 import heapq
 
 
-jobs = [[0, 3], [1, 9], [2, 6]]	
+jobs = [[0,2],[1,1],[4,3],[8,2],[9,5]]
+jobs = [[0, 3], [1, 9], [2, 6]]
 
-
+# 2 + 2 + 3 + 2 + 6 / 5 = 3 
 def solution(jobs) :
+    i = 0
     data = []
-    worktime = 0
-    current_time = 0
-    for job in jobs :
-        if len(data) >= 1 :
-            heapq.heappush(data, (job[1],job))
-        else : 
-            currentjob = job
-            worktime = current_time - currentjob[0] + currentjob[1]
-            print(worktime)
+    heapq.heappush(data , jobs[i])
+    i += 1
+    result= []
+    endtime = jobs[0][0]
+    while True :
+        if len(data) == 0 : 
+            current_work = jobs[i]
+            i += 1
+        else : current_work = heapq.heappop(data) 
+        endtime += current_work[1]
+        result.append(endtime - current_work[0])
+        print( result, data)
+        if i == len(jobs) -1 :
+            return result
+        while  i <= len(jobs)-1  & jobs[i][0] <= endtime :
+            heapq.heappush(data, jobs[i])
+            i += 1
+            # print(i, data)
+            
+    return result
+
 
 
 print(solution(jobs))
