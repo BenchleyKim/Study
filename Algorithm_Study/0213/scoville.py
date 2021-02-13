14a71fe13e7f6e352e64fe2bb237a73dfe17d9f7
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-02-13 15:07:30 +0900

    0213

---
+++ Algorithm_Study/0213/scoville.py
@@ -0,0 +1,22 @@
+from queue import PriorityQueue
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+def solution(scoville, K):
+    heap = PriorityQueue()
+    for s in scoville :
+        heap.put(s)
+    l = 0 
+    answer = 0
+    
+    while not heap.empty() :
+        f = heap.get()
+        if f >= K : 
+            return answer
+        s = heap.get()
+        l = f+s*2
+        answer += 1
+        heap.put(l)
+    return -1
+
+print(solution(scoville,K))
