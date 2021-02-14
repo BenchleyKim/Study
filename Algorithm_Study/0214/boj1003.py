1c00030c8b124bf7c8b625b591a6533c6c285ea2
bottlesteel97 <gch04602@naver.com>
2021-02-14 23:03:25 +0900

    0214

--- Algorithm_Study/0214/boj1003.py
+++ Algorithm_Study/0214/boj1003.py
@@ -1,22 +1,14 @@
-from sys import stdin
 
-# sys.stdin = open('./Algorithm_Study/0214/1003.txt', 'rt')
+def fibo(n) :
+  r5 = 5 ** 0.5
+  return int((((1+r5)**n)-((1-r5)**n))/((2**n)*r5))
 
-def fibo(n ) :
-  if n == 0 : 
-    global zerocount
-    zerocount += 1
-    return 0
-  elif n == 1 :
-    global onecount
-    onecount += 1
-    return 1
-  else : 
-    return fibo(n-1) + fibo(n-2)
-
-n = int(stdin.readline())
+n = int(input())
 for _ in range(n) :
   onecount = 0
   zerocount = 0
-  fibo(int(stdin.readline()))
-  print(zerocount, onecount)
+  k = int(input())
+  if k == 0 :
+    print("1 0")
+    continue
+  print(fibo(k-1), fibo(k))
