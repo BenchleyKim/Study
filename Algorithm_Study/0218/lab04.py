b41a5b23829097cfcc1163def681bb346881c865
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-02-18 17:46:32 +0900

    0218

--- Algorithm_Study/0218/lab04.py
+++ Algorithm_Study/0218/lab04.py
@@ -1,21 +1,12 @@
 import sys
-sys.stdin = open('./Algorithm_study/0218/lab04','r')
+sys.stdin = open('./Algorithm_study/0218/lab04test','r')
 
 N, K = map(int, input().split())
 stones = list(map(int,input().split()))
-print(stones)
-count = 0 
-while True :
-  count += 1
+mn = K
+for i in range(N-K+1) : 
   k = K
-  for i in range(N) : 
-    if k == 0 :
-      break
-    if stones[i] == 0 :
-      k -= 1
-      continue
-    stones[i] -= 1
-  if k == 0 :
-    print(count)
-    break
+  tmp = max(stones[i:i+K])
+  mn = min(mn,tmp )
+print(mn)
 
--- Algorithm_Study/0218/lab04test.py
+++ Algorithm_Study/0218/lab04test.py
@@ -1,2 +1,12 @@
 import random
-sys.stdout = open('./Algorithm_study/0218/lab04','r')
+import sys
+sys.stdout = open('./Algorithm_study/0218/lab04test','w')
+
+
+N = random.randrange(40000,100000)
+K = random.randrange(1,N)
+print(N, end = " ")
+print(K)
+for _ in range(N):
+  a = random.randrange(100,100000)
+  print(a,end = " ")
