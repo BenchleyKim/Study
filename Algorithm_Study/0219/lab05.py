d61d8642b94b0271e52efce744dcc7d1b34756fd
bottlesteel97 <gch04602@naver.com>
2021-02-19 16:32:16 +0900

    0219

--- Algorithm_Study/0219/lab05.py
+++ Algorithm_Study/0219/lab05.py
@@ -2,18 +2,16 @@ import sys
 sys.stdin = open("./Algorithm_Study/0219/lab05", "r")
 N = int(input())
 M = int(input())
-n = list(range(1,N+1))
+n = [1] * N
 arr = [0]*M
 arr.extend(n)
-print(arr)
 pos = list(range(M,N+M))
-print(pos)
 
-
+# 세그먼트 트리로 구간합 계산하고 업데이트하는 방식으로 계산 MlogN의 복잡도 
 for i in range(M) :
   dvd = int(input())
   idx = pos[dvd-1]
-  arr[M-i-1] = arr[idx]
+  arr[M-i-1] = 1
   arr[idx] = 0
   pos[dvd-1] = M-i-1
   print(arr)
