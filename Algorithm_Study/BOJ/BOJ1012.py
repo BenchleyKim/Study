73fd56834dcdd2e9f97d8161603a7937b3643ef5
bottlesteel97 <gch04602@naver.com>
2021-02-21 23:05:13 +0900

    Update BOJ1012.py
    TODO : BOJ1012

--- Algorithm_Study/BOJ/BOJ1012.py
+++ Algorithm_Study/BOJ/BOJ1012.py
@@ -8,7 +8,17 @@ for _ in range(T) :
   for _ in range(K) :
     X, Y= map(int,input().split())
     B[X][Y] = 1
+  count = 0
+  stack = []
+  checkList = [[0] * (N+1) for _ in range(M+1)]
+  for i in range(N+1):
+    for j in range(M+1):
+      if B[i][j] == 1 & checkList[i][j] == 0 :
+        tmp = stack.append()
+
+
   for i in B :
     print(i)
+
 
 
