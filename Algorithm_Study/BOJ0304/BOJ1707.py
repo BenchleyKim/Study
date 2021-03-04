66985bc101bda3a4cf2fa6dd228c7031dc8f9d6a
bottlesteel97 <gch04602@naver.com>
2021-03-04 16:30:02 +0900

    0304

--- Algorithm_Study/BOJ0304/BOJ1707.py
+++ Algorithm_Study/BOJ0304/BOJ1707.py
@@ -1,14 +1,19 @@
 import sys 
 sys.stdin = open("./Algorithm_Study/BOJ0304/BOJ1707", "r")
 
+# 싸이클이 있는지 확인하는 게 관건
 K = int(sys.stdin.readline())
 for _ in range(K) :
   V, E = map(int ,sys.stdin.readline().split())
-  graph = [[0] * (V+1) for _ in range(V+1)]
+  graph = {}
   for i in range(E) :
     start, end = map(int ,sys.stdin.readline().split())
-    graph[start][end] = 1
-    graph[end][start] = 1
-  for g in graph: 
-    print(g)
-    
+    if start in graph.keys() :
+      graph[start].append(end)
+    else :
+      graph[start] = [end]
+    if end in graph.keys() :
+      graph[end].append(start)
+    else :
+      graph[end] = [start]
+  print(graph)
