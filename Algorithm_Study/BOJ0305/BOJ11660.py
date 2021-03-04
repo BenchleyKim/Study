53170d8eed78a1748e1123f55af77ba0cef4b6ba
bottlesteel97 <gch04602@naver.com>
2021-03-05 01:14:06 +0900

    0305

---
+++ Algorithm_Study/BOJ0305/BOJ11660.py
@@ -0,0 +1,2 @@
+import sys 
+sys.stdin = open("./Algorithm_Study/BOJ0305/BOJ11660", "r")
--- Algorithm_Study/BOJ0305/BOJ11725.py
+++ Algorithm_Study/BOJ0305/BOJ11725.py
@@ -1,2 +1,29 @@
 import sys 
-sys.stdin = open("./Algorithm_Study/BOJ0304/BOJ11725", "r")
+sys.stdin = open("./Algorithm_Study/BOJ0305/BOJ11725", "r")
+
+N = int(sys.stdin.readline())
+check = [0] * (N+1)
+tree = {i:[] for i in range(1,N+1)}
+while True :
+  INPUT =  sys.stdin.readline()
+  if INPUT == '' :
+    break
+  s, e = map(int,INPUT.split())
+  tree[s].append(e)
+  tree[e].append(s)
+
+parents = [0] * (N+1)
+queue = [1]
+while queue :
+  tmp = queue.pop(0)
+  if check[tmp] :
+    continue
+  check[tmp] = 1
+  for i in tree[tmp] :
+    queue.append(i)
+    if parents[i] :
+      continue
+    parents[i] = tmp
+    
+for i in range(2,len(parents)):
+  print(parents[i])
