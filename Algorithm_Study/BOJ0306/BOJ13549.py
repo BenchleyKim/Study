a0444af92a799f42b0c70625f2173529d5a78dd4
bottlesteel97 <gch04602@naver.com>
2021-03-06 20:47:09 +0900

    0306

--- Algorithm_Study/BOJ0306/BOJ13549.py
+++ Algorithm_Study/BOJ0306/BOJ13549.py
@@ -1,22 +1,24 @@
 import sys 
 from collections import deque
+import heapq
 sys.stdin = open("./Algorithm_Study/BOJ0306/BOJ13549", "r")
 MAX = 100000
 N, K = map(int, sys.stdin.readline().split())
 
-queue = deque([(N,0)])
+queue = [(0,N)]
+
 check = [0] * MAX * 2
 while queue :
-  tmp , count = queue.popleft()
+  count, tmp = heapq.heappop(queue)
   if tmp == K : 
     print(count)
     break
-  if tmp >= 2*MAX or tmp <= 0 : 
+  if tmp >= 2*MAX or tmp < 0 : 
     continue
   if check[tmp] == 1 :
     continue
   check[tmp] = 1
-  queue.append((2*tmp,count))
-  queue.append((tmp-1,count+1))
-  queue.append((tmp+1,count+1))
-  
+  heapq.heappush(queue, (count,2*tmp))
+  heapq.heappush(queue, (count+1,tmp-1))
+  heapq.heappush(queue, (count+1,tmp+1))
+
