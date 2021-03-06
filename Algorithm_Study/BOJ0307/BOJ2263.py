14893dc7a9047058a3228c190eaad8a5a3160fe0
bottlesteel97 <gch04602@naver.com>
2021-03-07 01:37:33 +0900

    0307

---
+++ Algorithm_Study/BOJ0307/BOJ2263.py
@@ -0,0 +1,10 @@
+import sys 
+sys.stdin = open("./Algorithm_Study/BOJ0307/BOJ2263", "r")
+
+N = sys.stdin.readline()
+INORDER = list(map(int, sys.stdin.readline().split()))
+POSTORDER = list(map(int, sys.stdin.readline().split()))
+left = []
+right = []
+
+for i in range(N) :
