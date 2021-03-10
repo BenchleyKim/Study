08baa6c9dab73be970ad2ee84aef201ea8f79ae5
bottlesteel97 <gch04602@naver.com>
2021-03-11 03:06:11 +0900

    0311

--- Algorithm_Study/BOJ0310/BOJ1786.py
+++ Algorithm_Study/BOJ0310/BOJ1786.py
@@ -1,20 +1,48 @@
 import sys 
 sys.stdin = open("./Algorithm_Study/BOJ0310/BOJ1786", "r")
 
+
+def getpartialmatch(N): 
+  m=len(N) 
+  pi=[0 for i in range(m)] 
+  begin=1 
+  matched=0 
+  while begin+matched<m: 
+    if N[begin+matched]==N[matched]: 
+      matched+=1 
+      pi[begin+matched-1]=matched 
+    else: 
+      if matched==0: 
+        begin+=1 
+      else: 
+        begin+=matched-pi[matched-1] 
+        matched=pi[matched-1] 
+  return pi 
+
+def KMP(H, N): 
+  n=len(H) 
+  m=len(N) 
+  ret=[] 
+  pi=getpartialmatch(N) 
+  begin=0 
+  matched=0 
+  while begin<=n-m: 
+    if matched<m and H[begin+matched]==N[matched]: 
+      matched+=1 
+      if matched==m: 
+        ret.append(begin) 
+    else: 
+      if matched==0: 
+        begin+=1 
+      else: 
+        begin+=matched-pi[matched-1] 
+        matched=pi[matched-1] 
+  return ret 
+
 T = sys.stdin.readline().rstrip()
 P = sys.stdin.readline().rstrip()
-print(P)
-print(len(P))
-pl = len(P)
-tl = len(T)
-F = [0] * pl 
-for i in range(pl) :
-  j = i-1
-  while j >= 0 :
-    print(P[:j], P[i-j:i])
-    if P[:j] == P[i-j:i] :
-      F[i] = j
-      break
-    j -= 1
-  print(F)
-  
+
+x=KMP(T, P) 
+print(len(x)) 
+for i in x: print(i+1, end=" ")
+
--- Algorithm_Study/BOJ0310/BOJ1865.py
+++ Algorithm_Study/BOJ0310/BOJ1865.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("./Algorithm_Study/BOJ0310/BOJ1865", "r")
 
 read = sys.stdin.readline
-
+INF = sys.maxsize
 TC = int(read())
 for _ in range(TC) :
   N , M , W= map(int, read().split())
@@ -18,7 +18,23 @@ for _ in range(TC) :
   for i in range(W) :
     S , E, T = map(int, read().split())
     graph[S][E] = -T
-  print(graph)
+  result = [INF for _ in range(N+1)]
+  result[1] = 0
+  for _ in range(2*M+W) :
+    for node in graph.keys() :
+      for sub in graph[node].keys() :
+        if result[sub] > result[node] + graph[node][sub] : 
+          result[sub] = result[node] + graph[node][sub]
+  Flag = False
+  for node in graph.keys() :
+    if Flag : break
+    for sub in graph[node].keys() :
+      if result[sub] > result[node] + graph[node][sub] :
+        Flag = True
+        break
+  if Flag : print("YES")
+  else : print("NO")
+      
+      
 
 
-print(graph)
