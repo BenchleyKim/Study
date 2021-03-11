59b0221f9a680b136d054732069b6aae6ad2d5a4
bottlesteel97 <gch04602@naver.com>
2021-03-11 23:23:31 +0900

    0311

--- Algorithm_Study/BOJ0311/BOJ1197.py
+++ Algorithm_Study/BOJ0311/BOJ1197.py
@@ -8,31 +8,22 @@ V , E = map(int , sys.stdin.readline().split())
 for _ in range(E) :
   A, B ,C = map(int ,sys.stdin.readline().split())
   heapq.heappush(heap, (C,A,B))
-check = [0] * (V+1)
-weight = [0] * (V+1)
+check = [v for v in range(V+1)]
 ans = 0
-C, A ,B = heapq.heappop(heap)
-ans = C 
-check[A] = check[B] = A
-print(A,B,check)
+
+def find_root(node) :
+  tmp = node
+  while check[node] != node :
+    node = check[node]
+  check[tmp] = node
+  return node
 
 while heap :
   C, A, B = heapq.heappop(heap)
-  if check[A] == check[B] and check[A] != 0 and check[B] != 0:
+  if find_root(A) == find_root(B) :
     continue
+  check[find_root(A)] = find_root(B)
   ans += C
-  print(A, B,check)
-  if check[A] == 0 and check[B] == 0 :
-    check[A] = check[B] = A
-    continue
-  if check[A] == 0 :
-    check[A] = check[B]
-    continue
-  if check[B] == 0 :
-    check[B] = check[A]
-    continue
-  check[A] = check[B]
-
 
 print(ans)
 
