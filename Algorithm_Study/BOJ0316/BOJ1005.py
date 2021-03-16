86fa99f038c477f6bb4033fb5a09cabc1508a6d9
bottlesteel97 <gch04602@naver.com>
2021-03-16 12:23:09 +0900

    0316

---
+++ Algorithm_Study/BOJ0316/BOJ1005.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("./Algorithm_Study/BOJ0316/BOJ1005", "r")
+
+T = int(sys.stdin.readline())
+for _ in range(T) :
+  N, K = map(int, sys.stdin.readline().split())
+  arr = list(map(int, sys.stdin.readline().split()))
+  check = { i+1 : 0 for i in range(N)}
+  graph = {i+1:[] for i in range(N)}
+  for i in range(K) :
+    X, Y = map(int, sys.stdin.readline().split())
+    graph[X].append(Y)
+    check[Y] += 1
+  stack = []
+  for i in check.keys() :
+    if check[i] == 0 :
+      stack.append(i)
+  print(stack)
+  print(graph)
+  while stack :
+    tmp = stack.pop()
+    
+  print(check)
+  target = int(sys.stdin.readline())
+
+
