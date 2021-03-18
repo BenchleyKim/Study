cd1a25df67c201e74f5b251c53df46a73a601757
bottlesteel97 <gch04602@naver.com>
2021-03-18 12:42:12 +0900

    0318

--- Algorithm_Study/BOJ0317/BOJ1806.py
+++ Algorithm_Study/BOJ0317/BOJ1806.py
@@ -3,21 +3,24 @@ sys.stdin = open("./Algorithm_Study/BOJ0317/BOJ1806", "r")
 
 N , S = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
-print(arr)
+# print(arr)
 left = 0 
-right = 1
+right = 0
 
 interSum = arr[0]
-ans = N
+ans = N+2
 
-while right < N-1 and left <= right :
-  print(left, right , interSum)
+while right <= N-1 and left <= right :
   if interSum >= S :
     interSum -= arr[left]
     left += 1 
-    ans = min(ans, right-left)
+    ans = min(ans, right-left+2)
   else :
     right += 1
+    if right > N-1 :
+      break
     interSum += arr[right]
-
-print(ans)
+if ans == N+2 :
+  print(0)
+else :
+  print(ans)
