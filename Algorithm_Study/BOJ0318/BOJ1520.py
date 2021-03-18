4d3166621d61b293a8863d45c190564c51dcf005
bottlesteel97 <gch04602@naver.com>
2021-03-19 00:29:59 +0900

    0319

--- Algorithm_Study/BOJ0318/BOJ1520.py
+++ Algorithm_Study/BOJ0318/BOJ1520.py
@@ -1,30 +1,31 @@
 import sys
 sys.stdin = open("./Algorithm_Study/BOJ0318/BOJ1520", "r")
 input = sys.stdin.readline
-M, N = map(int, input().split() )
-table = []
-for i in range(M) :
-  table.append(list(map(int,input().split())))
-for t in table :
-  print(t)
-DP = [[0]* N for i in range(M)]
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-stack  = [(0,0,1)]
-while stack :
-  cx, cy,w = stack.pop()
-  print(cx,cy,w)
-  DP[cx][cy] += w
-  for i in range(4) :
-    nx , ny = cx+dx[i], cy+dy[i]
-    if nx < 0 or nx >= M or ny < 0 or ny >= N :
-      continue
-    if table[nx][ny] < table[cx][cy] :
-      stack.append([nx,ny,DP[cx][cy]])
-for d in DP :
-  print(d)
-print(DP[M-1][N-1])
+sys.setrecursionlimit(10000)
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+def dfs(x, y):
+    if x == 0 and y == 0:
+        return 1
+    for d in dp :
+      print(d)
+    print()
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if s[x][y] < s[nx][ny]:
+                    dp[x][y] += dfs(nx, ny)
+    return dp[x][y]
+m, n = map(int, input().split())
+s = [list(map(int, input().split())) for i in range(m)]
+dp = [[-1] * n for i in range(m)]
 
+print(dfs(m - 1, n - 1))
+for d in dp :
+    print(d)
 
     
 
