1d9a1e60b2c26289a16aab96962d0a59e2c021ce
bottlesteel97 <gch04602@naver.com>
2021-03-29 19:09:51 +0900

    0329

---
+++ Algorithm_Study/BOJ0329/BOJ2174.py
@@ -0,0 +1,3 @@
+import sys 
+sys.stdin = open("./Algorithm_Study/BOJ0329/BOJ2174", "r")
+input = sys.stdin.readline
--- Algorithm_Study/BOJ0329/BOJ5710.py
+++ Algorithm_Study/BOJ0329/BOJ5710.py
@@ -7,8 +7,8 @@ def calbill(K) :
   if K <= 10000 :
     return (K-100)*3+200
   if K <= 1000000 :
-    return (K-100-10000)*5+200+30000
-  return (K-100-10000-1000000)*7+200+30000+5000000
+    return (K-10000)*5+29900
+  return (K-1000000)*7+4979900
 
 def callwatt(K) :
   if K <= 200 :
@@ -19,58 +19,36 @@ def callwatt(K) :
     ans += 100
   if K > 29700 :
     K -= 29700
-    ans += 10000
+    ans += 9900
   else :
     return ans + K//3
   if K > 4950000 :
     K -= 4950000
-    ans += 1000000
+    ans += 990000
   else :
     return ans+K//5
   return ans+K//7
 
 
-
-print("callwatt")
-print(callwatt(2900))
-
-print(callwatt(35515))
-
-print()
-print(calbill(1000))
-print(calbill(10223))
-
-print(calbill(10223)-calbill(1000))
-
-
-# def dac(left, right) :
-#   mid = (left+right) //2
-#   neighbor = total_watt - mid
-#   diff = calbill(neighbor) - calbill(mid)
-#   print(left, mid , right , diff)
-#   if left == right :
-#     print(calbill(right))
-#     return
-#   if diff < B :
-#     dac(left+1, mid)
-#   if diff > B :
-#     dac(mid,right)
+def dac(left, right) :
+  mid = (left+right) //2
+  neighbor = total_watt - mid
+  diff = calbill(neighbor) - calbill(mid)
+  if diff == B :
+    print(calbill(mid))
+    return
+  if diff < B :
+    dac(left, mid-1)
+  if diff > B :
+    dac(mid+1,right)
 
 
-# while True :
-#   A, B = map(int, input().split())
-#   total_watt = 0
-#   if A == 0 and B == 0 :
-#     break
-#   if A <= 200 :
-#     total_watt = A//2
-#   elif A <= 29900 :
-#     total_watt = (A-200)//3+100
-#   elif A <= 4979900 : 
-#     total_watt = (A-29900)//5+10000+100
-#   else :
-#     total_watt = (A -4979900)//7+1000000+10000+100
-#   print(total_watt) 
-#   dac(0,total_watt)
+while True :
+  A, B = map(int, input().split())
+  
+  if A == 0 and B == 0 :
+    break
+  total_watt = callwatt(A)
+  dac(0,total_watt)
 
   
