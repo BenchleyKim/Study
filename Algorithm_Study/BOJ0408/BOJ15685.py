a8f2c0eeb1991ddb938891f3a26859588e31bb99
bottlesteel97 <gch04602@naver.com>
2021-04-08 19:44:01 +0900

    Update BOJ15685.py

--- Algorithm_Study/BOJ0408/BOJ15685.py
+++ Algorithm_Study/BOJ0408/BOJ15685.py
@@ -1,7 +1,27 @@
 import sys
 sys.stdin = open("./Algorithm_Study/BOJ0408/BOJ15685", "r")
-input = sys.stdin.readline
 
-N = int(input())
-board = [[0] * 100 for _ in range(100)]
-for i in range()
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+n = int(input())
+s = [[0] * 101 for i in range(101)]
+for i in range(n):
+    y, x, d, g = map(int, input().split())
+    s[x][y] = 1
+    temp = [d]
+    q = [d]
+    for _ in range(g + 1):
+        for k in q:
+            x += dx[k]
+            y += dy[k]
+            s[x][y] = 1
+        q = [(i + 1) % 4 for i in temp]
+        q.reverse()
+        temp = temp + q
+        print(temp)
+result = 0
+for i in range(100):
+        for j in range(100):
+            if s[i][j] and s[i][j + 1] and s[i + 1][j] and s[i + 1][j + 1]:
+                result += 1
+print(result)
