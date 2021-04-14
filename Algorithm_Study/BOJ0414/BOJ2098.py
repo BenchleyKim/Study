b4ac5ecd9fbc6d5ed388309c05bbd486c418a460
bottlesteel97 <gch04602@naver.com>
2021-04-14 17:52:50 +0900

    0414

--- Algorithm_Study/BOJ0414/BOJ2098.py
+++ Algorithm_Study/BOJ0414/BOJ2098.py
@@ -2,14 +2,23 @@ import sys
 sys.stdin = open("./Algorithm_Study/BOJ0414/BOJ2098","r")
 input = sys.stdin.readline
 
-N = int(input())
-board = []
-for i in range(N) :
-    board.append(list(map(int, input().split())))
-for b in board :
-    print(b)
-graph = { i : {} for i in range(1,N+1)}
-for i in range(N) :
-    for j in range(N) :
-        if board[i][j] :
-            graph[i+1][j+1] = board[i][j]
+def tsp(idx, path): 
+    if path == total: 
+        return table[idx][0] if table[idx][0] > 0 else 20202020 
+    if memo[idx][path] > 0: 
+        return memo[idx][path] 
+    tmp = 20202020 
+    for i in range(1, n): 
+        if (path >> i)%2 == 1 or table[idx][i] == 0: continue 
+        tmp = min(tmp, table[idx][i] + tsp(i, path|(1<<i))) 
+    memo[idx][path] = tmp 
+    return tmp 
+n = int(input()) 
+table = [list(map(int, input().split())) for _ in range(n)] 
+memo = [[0]*(1<<n) for _ in range(n)] 
+total = (1<<n)-1 
+for m in memo :
+    print(m)
+print(tsp(0, 1))
+for m in memo :
+    print(m)
