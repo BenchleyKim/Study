79a625a3e7d23625baf4f8f3173ea2ccc4f2e90b
bottlesteel97 <gch04602@naver.com>
2021-04-16 16:07:56 +0900

    Update BOJ2448.py

--- Algorithm_Study/BOJ0416/BOJ2448.py
+++ Algorithm_Study/BOJ0416/BOJ2448.py
@@ -1,24 +1,22 @@
 import sys
+import math
 sys.stdin = open("./Algorithm_Study/BOJ0416/BOJ2448","r")
 
-N = int(input())
-tmp = N//3
-k = 0 
-while tmp > 1 :
-    tmp //= 2
-    k += 1
-print(k) 
-def triangle(left, right) :
-    if right - left == 5 :
-        print(" " * left + " "* 2 + "*")
-        print()
-        print(" "* left +" "+"* *")
-        print()
-        print(" "* left +"*****")
-        return
-    else :
-        triangle(((right-left)//4)*2,((right-left)//4)*3)
-        triangle(0,(right-left)//2)
-        triangle((right-left)//2,right)
+def draw(cal):
+    for i in range(len(stars)):
+        stars.append(stars[i] + stars[i])
+        stars[i] = ('   ' * cal + stars[i] + '   ' * cal)
 
-triangle(0,48)
+
+stars = ['  *   ', ' * *  ', '***** ']
+N = int(input())
+
+iter = int(math.log(N // 3, 2))
+print(iter)
+
+for i in range(iter):
+    draw(int(pow(2, i)))
+    print(stars)
+
+for i in range(N):
+    print(stars[i])
