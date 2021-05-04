268147a0b0eb359c21f57b820d5c622fa7e5d0ce
bottlesteel97 <gch04602@naver.com>
2021-05-04 22:44:43 +0900

    Update BOJ14003.py

--- Algorithm_Study/BOJ0504/BOJ14003.py
+++ Algorithm_Study/BOJ0504/BOJ14003.py
@@ -2,10 +2,35 @@ import sys
 sys.stdin = open(".\Algorithm_Study\BOJ0504\BOJ14003","r")
 input = sys.stdin.readline
 
-N = int(input())
-A = list(map(int, input().split()))
 
 
 
+import bisect
+
+
+def lis(arr):
+    lis_arr = [arr[0]]
+    res = [0]
+    for n in arr[1:]:
+        if lis_arr[-1] < n:
+            lis_arr.append(n)
+            res.append(len(lis_arr)-1)
+        else:
+            where = bisect.bisect_left(lis_arr, n)
+            lis_arr[where] = n
+            res.append(where)
+
+    i = len(lis_arr)
+    print(i)
+    ans = []
+    for idx in range(len(res)-1, -1, -1):
+        if res[idx] == i-1:
+            ans.append(arr[idx])
+            i -= 1
+    print(*ans[::-1])
+
+
+input()
+lis(list(map(int, input().split())))
 
 
