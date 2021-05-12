890eff1be97280067b572e41a2e070f3a66d7c04
bottlesteel97 <gch04602@naver.com>
2021-05-12 22:46:39 +0900

    Update BOJ11054.py

--- Algorithm_Study/BOJ0512/BOJ11054.py
+++ Algorithm_Study/BOJ0512/BOJ11054.py
@@ -2,5 +2,21 @@ import sys
 sys.stdin = open(".\Algorithm_Study\BOJ0512\BOJ11054","r")
 input = sys.stdin.readline
 
-N = int(input())
-arr = list(map(int, input().split()))
+n = int(input())
+a = list(map(int, input().split()))
+dpp = [0 for i in range(n)]
+dpm = [0 for i in range(n)]
+dpb = [0 for i in range(n)]
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j] and dpp[i] < dpp[j]:
+            dpp[i] = dpp[j]
+    dpp[i] += 1
+for i in range(n - 1, -1, -1):
+    for j in range(n - 1, i, -1):
+        if a[i] > a[j] and dpm[i] < dpm[j]:
+            dpm[i] = dpm[j]
+    dpm[i] += 1
+for i in range(n):
+    dpb[i] = dpp[i] + dpm[i] - 1
+print(max(dpb))
