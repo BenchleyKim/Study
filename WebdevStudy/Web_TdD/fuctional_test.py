c7e36f8d9b6e25d140f0a3a665d1d3b0df274343
bottlesteel97 <gch04602@naver.com>
2021-06-07 00:05:10 +0900

    최소 HTML 뷰를 반환하는 기본 뷰

--- WebdevStudy/Web_TdD/fuctional_test.py
+++ WebdevStudy/Web_TdD/fuctional_test.py
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase) :
     def test_can_start_a_list_and_retrieve_it_later(self) :
 
         self.brower.get('http://localhost:8000')
-        self.assertIn('To_Do', self.brower.title )
+        self.assertIn('To-Do', self.brower.title )
         self.fail("Finished the test!")
 
 if __name__ == '__main__' :
--- WebdevStudy/Web_TdD/superlists/lists/views.py
+++ WebdevStudy/Web_TdD/superlists/lists/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
 
-def home_page() :
-    pass
+def home_page(request) :
+    return HttpResponse('<html><title>To-Do lists</title></html>')
