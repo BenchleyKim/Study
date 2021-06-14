655f619b69250542606dc8ed309e5259e0c665de
bottlesteel97 <gch04602@naver.com>
2021-06-14 21:42:42 +0900

    0614

---
+++ Algorithm_Study/BOJ0614/BOJ4836.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open(".\Algorithm_Study\BOJ0614\BOJ4836","r")
+input = sys.stdin.readline
+
+def check(str: string ) :
+    # rule 1 
+    if "dip" in string :
+        
+
+tmp = input().rstrip()
+
+while tmp != "" :
+    tmp = input().rstrip()
+    print(tmp)
+
