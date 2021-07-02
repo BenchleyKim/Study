4a4ec72a986eaece069218d71993a43d96bbb878
bottlesteel97 <gch04602@naver.com>
2021-07-02 20:25:27 +0900

    Create PGM42840.py

---
+++ Algorithm_Study/PGM0702/PGM42840.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    par_1 = [1,2,3,4,5] * 2_000
+    par_2 = [2, 1, 2, 3, 2, 4, 2, 5] * 1_250
+    par_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 500
+    pars = [0,0,0]
+    for i in range(len(answers)) :
+        if answers[i] == par_1[i] :
+            pars[0] += 1
+        if answers[i] == par_2[i] : 
+            pars[1] += 1
+        if answers[i] == par_3[i] : 
+            pars[2] += 1
+    mx_idx = 0
+    for i in range(3) :
+        if pars[i] >
+            
+            
+    answer = []
+    return answer
